ca77af8f65c3f5ab80e8b2e962236c98681de102
DiegoAgher <diego.aguado@wizeline.com>
2017-12-05 23:08:22 -0600

    add script to build_spectrograms

---
+++ build_spectrograms.py
@@ -0,0 +1,62 @@
+from utils.load_transform_data import dismiss_shorter_tracks,\
+    filter_short_files_and_ids, get_target_variable_for_windows_categorical
+from utils.load_transform_data import get_target_variables_per_group,\
+    create_windows_from_spectrogram
+from load_small_dataset import tracks, train_valid_tracks, x_train_ids,\
+    validation_valid_tracks, x_val_ids, test_valid_tracks, x_test_ids
+
+
+def build_spectrograms(hop_length=350, length_threshold=27):
+
+    spectrograms, shorter_files =\
+        dismiss_shorter_tracks(train_valid_tracks, hop_length,
+                               length_threshold)
+
+    filtered_train_ids = filter_short_files_and_ids(x_train_ids,
+                                                    shorter_files)
+
+    spectrograms_val, shorter_files_val =\
+        dismiss_shorter_tracks(validation_valid_tracks, hop_length,
+                               length_threshold)
+
+    filtered_val_ids = filter_short_files_and_ids(x_val_ids,
+                                                  shorter_files_val)
+
+    spectrograms_test, shorter_files_test =\
+        dismiss_shorter_tracks(test_valid_tracks, hop_length,
+                               length_threshold)
+
+    filtered_test_ids = filter_short_files_and_ids(x_test_ids,
+                                                   shorter_files_val)
+
+    y_train, y_val, y_test =\
+        get_target_variables_per_group(tracks, filtered_train_ids,
+                                       filtered_val_ids, filtered_test_ids)
+
+    small_data_set_spectograms = {}
+    train_sequences = create_windows_from_spectrogram(spectrograms)
+    y_train_binary_sequences =\
+        get_target_variable_for_windows_categorical(y_train)
+
+    small_data_set_spectograms['train'] = (train_sequences,
+                                           y_train_binary_sequences)
+
+    window_size = train_sequences.shape[1]
+
+    val_sequences = create_windows_from_spectrogram(spectrograms_val,
+                                                    window_size=window_size)
+    y_val_binary_sequences = get_target_variable_for_windows_categorical(y_val)
+
+    small_data_set_spectograms['val'] = (val_sequences,
+                                         y_val_binary_sequences)
+
+    test_sequences = create_windows_from_spectrogram(spectrograms_test,
+                                                     window_size=window_size)
+    y_test_binary_sequences =\
+        get_target_variable_for_windows_categorical(y_test)
+
+    small_data_set_spectograms['test'] = (test_sequences,
+                                          y_test_binary_sequences)
+
+    return small_data_set_spectograms
+
