700092081e3a5de21b06710ccd39ee48e74ad3e9
DiegoAgher <diego.aguado@wizeline.com>
2017-12-06 23:28:19 -0600

    evaluate dataset by averaging probabilities

--- training/Dielmann_windows.py
+++ training/Dielmann_windows.py
@@ -1,8 +1,14 @@
-from utils.load_transform_data impor from_segments_to_song
+import numpy as np
+from utils.load_transform_data import average_window_array,\
+    to_categorical_inverse, enc
 from models.Dielmann import DielmannArq
 from build_spectrograms import build_spectrograms
 
-small_dataset_spectrograms = build_spectrograms()
+
+windows_number = 10
+songs_size = 30
+
+small_dataset_spectrograms = build_spectrograms(windows_number, 600)
 
 train_sequences, y_train_binary_sequences = small_dataset_spectrograms['train']
 val_sequences, y_val_binary_sequences = small_dataset_spectrograms['val']
@@ -22,8 +28,20 @@ loss, accuracy = classifier.model.evaluate(test_sequences,
                                            y_test_binary_sequences)
 
 
+print("Loss: {} ; Acc: {} on windowed test dataset ".format(loss, accuracy))
 
-print("Loss: {} ; Acc: {} on test dataset".format(loss, accuracy))
+window_probabilities = classifier.model.predict(test_sequences)
+
+averaged_probabilities = average_window_array(window_probabilities)
+predictions = np.argmax(averaged_probabilities, axis=1)
+
+y_test = to_categorical_inverse(y_test_binary_sequences)
+indices = [windows_number * x for x in range(y_test.shape[0] / windows_number)]
+y_test = y_test[indices]
+
+accuracy = sum(predictions == y_test) / len(y_test)
+
+print("Accuracy for full songs is {}".format(accuracy))
 
 classifier.model.save('my_classifier.h5')
 
