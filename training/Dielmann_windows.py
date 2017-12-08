use right dimensions for spectograms in model and fix dimensionality in averaging array util

training/Dielmann_windows.py
@@ -8,15 +8,18 @@ from build_spectrograms import build_spectrograms
 windows_number = 10
 songs_size = 30
 
-small_dataset_spectrograms = build_spectrograms(windows_number, 600)
+small_dataset_spectrograms = build_spectrograms(windows_number, 350)
 
 train_sequences, y_train_binary_sequences = small_dataset_spectrograms['train']
 val_sequences, y_val_binary_sequences = small_dataset_spectrograms['val']
 test_sequences, y_test_binary_sequences = small_dataset_spectrograms['test']
 
+frame_size = train_sequences.shape[1]
 
-classifier = DielmannArq(frames=377, nb_filters_1=128, nb_filters_2=128*2,
-                         dense_size=1024, dropout_prob_1=0.2, nb_classes=4)
+
+classifier = DielmannArq(frames=frame_size, nb_filters_1=128,
+                         nb_filters_2=128*2, dense_size=1024,
+                         dropout_prob_1=0.2, nb_classes=4)
 
 classifier.build_convolutional_model()
 classifier.model.fit(train_sequences, y_train_binary_sequences,
@@ -36,7 +39,9 @@ averaged_probabilities = average_window_array(window_probabilities)
 predictions = np.argmax(averaged_probabilities, axis=1)
 
 y_test = to_categorical_inverse(y_test_binary_sequences)
-indices = [windows_number * x for x in range(y_test.shape[0] / windows_number)]
+number_songs = int(y_test.shape[0] / windows_number)
+
+indices = [windows_number * x for x in range(number_songs)]
 y_test = y_test[indices]
 
 accuracy = sum(predictions == y_test) / len(y_test)

utils/load_transform_data.py
@@ -108,10 +108,10 @@ def get_target_variable_for_windows_categorical(target_variable,
 
 def average_window_array(array, nb_windows=10):
     averages = []
-    samples_number = array.shape[0]
+    samples_number = int(array.shape[0] / nb_windows)
     for index in range(0, samples_number):
         averages.append(array[index * nb_windows:
-                                 (index + 1) * nb_windows].mean(axis=0))
+                              (index + 1) * nb_windows].mean(axis=0))
 
     return np.array(averages)
 