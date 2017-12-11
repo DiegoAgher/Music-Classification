254b31f5360f311673451a5702869d8c81ba0a3e
DiegoAgher <diego.aguado@wizeline.com>
2017-12-10 21:34:19 -0600

    change cmap

--- utils/plotting.py
+++ utils/plotting.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
-                          cmap=plt.cm.Blues):
+                          cmap=plt.get_cmap('Reds')):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
